6ec1ef143be86779925dca83b29784d72c51ea88
Riley Ren <renxuebin@kongming-inc.com>
2014-12-12 14:59:36 +0800

    分辨率大于640的时候字体最大限制为1.5

--- responsive.js
+++ responsive.js
@@ -4,7 +4,12 @@
         html = doc.documentElement;
     function setFontSize() {
         var htmlWidth = html.getBoundingClientRect().width;
-        win.rem = htmlWidth / 640 * 1.5, html.style.fontSize = win.rem + "rem";
+        if(htmlWidth < 640) {
+            win.rem = htmlWidth / 640 * 1.5;
+        } else {
+            win.rem = 1.5;
+        }
+        html.style.fontSize = win.rem + "rem";
     }
     win.addEventListener("resize", function() {
         clearTimeout(timeout), timeout = setTimeout(setFontSize, 300);
